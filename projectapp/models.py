ab4d9e49671aafaf7e179cce0800e4c019c99158
rougevin <rougevin@sk.com>
2022-03-31 15:19:03 +0900

    django course 40 commit

---
+++ projectapp/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Project(models.Model):
+    opener = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='project')
+    title = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='project/', null=True)
+    created_at = models.DateTimeField(auto_now=True)
